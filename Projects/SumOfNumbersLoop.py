e4ba6ba660bec25a36843f2c4ff56888533fca50
= <sergeikoblov2000@gmail.com>
2024-11-11 13:58:39 +0000

    Practice Project: Sum of Numbers with a For Loop
    Write a program that calculates the sum of all numbers from 1 to 100 using a for loop.

---
+++ Projects/SumOfNumbersLoop.py
@@ -0,0 +1,21 @@
+"""
+Objective:
+
+Write a program that calculates the sum of
+all numbers from 1 to 100 using a for loop.
+
+"""
+
+# --------- my attempt ---------
+sumCount = 0
+
+for i in range(100):
+    sumCount += i
+    print(sumCount)
+
+
+# --------- correct answer ---------
+total = 0
+for i in range(1, 101):
+    total += i
+print("The sum is:", total)
