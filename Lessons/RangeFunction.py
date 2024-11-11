395d381900ceaeb1435ef311112dadaaa17249b8
= <sergeikoblov2000@gmail.com>
2024-11-11 13:47:41 +0000

    Learned Range():
    * Range() Function

---
+++ Lessons/RangeFunction.py
@@ -0,0 +1,13 @@
+# range() = generates a sequence of numbers
+# range(start, end, step)
+
+# prints 1 to 5
+for i in range(1, 6):
+    print(f"Current number: {i}")
+
+print()
+print()
+
+# prints 1 to 20, but skips over a number (step = 2)
+for i in range(1, 20, 2):
+    print(f"Current number: {i}")
