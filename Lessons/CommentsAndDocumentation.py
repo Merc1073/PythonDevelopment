99caaeeccb88ab2d7f3ad0a26ecfa72619c3bda4
= <sergeikoblov2000@gmail.com>
2024-11-11 12:48:32 +0000

    Learned Comments and Documentation:
    * Single line comments
    * Multi-line comments

---
+++ Lessons/CommentsAndDocumentation.py
@@ -0,0 +1,8 @@
+# This is an example of a single line comment
+print("The statement above me is a comment, it will not do anything in code.")
+
+"""
+This is an example of a multi-line comment.
+It allows you to create multiple lines of comments.
+Useful for longer documentations or explanations.
+"""
