bc3917e9b0a92c3a38365fae49287aa0041d46c4
= <sergeikoblov2000@gmail.com>
2024-11-11 12:50:09 +0000

    Learned Type Casting/Conversion

---
+++ Lessons/TypeCasting.py
@@ -0,0 +1,18 @@
+floatNum = 1.8
+print(floatNum)
+
+# convert to integer
+floatNum = int(floatNum)
+print(floatNum)
+
+# convert to float
+floatNum = float(floatNum)
+print(floatNum)
+
+# convert to string
+floatNum = str(floatNum)
+print(floatNum)
+
+# convert to boolean (non-zero numbers/non-empty = True)
+floatNum = bool(floatNum)
+print(floatNum)
