7d8eb5b8b8c6250217102219c387518b12f0451d
= <sergeikoblov2000@gmail.com>
2024-11-11 12:59:12 +0000

    Practice Project: Convert and Add Numbers
    Have the user input numbers as strings, convert then to integers, and print out their sum when added.

---
+++ Projects/ConvertAndAddNumbers.py
@@ -0,0 +1,9 @@
+string1 = str(input("Enter your first number: "))
+string2 = str(input("Enter your second number: "))
+
+string_to_num1 = int(string1)
+string_to_num2 = int(string2)
+
+total = string_to_num1 + string_to_num2
+
+print(f"{string_to_num1} + {string_to_num2} = {total}.")
