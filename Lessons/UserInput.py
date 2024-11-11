6c7d87106f55e42ec18a149afe9047680ee94ba8
= <sergeikoblov2000@gmail.com>
2024-11-11 12:49:22 +0000

    Learned User Input:
    * input() function

---
+++ Lessons/UserInput.py
@@ -0,0 +1,9 @@
+# input() allows user to input data, result is stored inside variable
+name = input("Enter your name: ")
+
+# Can type cast user's input to a specific data type
+# User is entering a whole number, therefore type cast it to an integer
+age = int(input("Enter your age: "))
+
+# Print out name and age that user input
+print(f"Hello {name}, you are {age} years old.")
