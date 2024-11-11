4498db67bee99eb452d14710c230d51389a7c8e1
= <sergeikoblov2000@gmail.com>
2024-11-11 12:57:55 +0000

    Practice Project: Find the Remainder
    Create two integer variables, and use the modulo operator to find the remainder, and output it to the console.

---
+++ Projects/FindRemainder.py
@@ -0,0 +1,6 @@
+dividend = int(input("Enter your dividend: "))
+divisor = int(input("Enter your divisor: "))
+
+remainder = dividend % divisor
+
+print(f"The remainder of {dividend} / {divisor} = {remainder}")
