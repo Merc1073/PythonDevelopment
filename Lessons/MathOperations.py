4e01829d1671f0f9b7b1e0736803ec5a9dbf84b4
= <sergeikoblov2000@gmail.com>
2024-11-11 12:47:38 +0000

    Learned Basic Math Operations:
    * Addition
    * Subtraction
    * Multiplication
    * Division
    * Floor Division
    * Modulus
    * Exponentiation

---
+++ Lessons/MathOperations.py
@@ -0,0 +1,30 @@
+x = 4
+y = 6
+
+
+# Type casting int variables to float
+x = float(x)
+y = float(y)
+
+
+
+# Addition
+print(x + y)
+
+# Subtraction
+print(x - y)
+
+# Multiplication
+print(x * y)
+
+# Division
+print(x / y)
+
+# Floor division (Rounds down to nearest whole number OR lowest negative number)
+print(x // y)
+
+# Modulus (remainder)
+print(x % y)
+
+# Exponentiation (to the power of)
+print(x ** y)
