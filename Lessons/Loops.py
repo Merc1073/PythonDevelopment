91f619fce0665097f87804becc574c855c9e146e
= <sergeikoblov2000@gmail.com>
2024-11-11 13:43:29 +0000

    Learned Loops:
    * For Loop
    * While Loop

---
+++ Lessons/Loops.py
@@ -0,0 +1,21 @@
+word = "Brilliant"
+
+for char in word:
+    print(char, end=" ")
+
+print()
+print()
+
+numList = [1, 2, 3, 4, 5]
+
+for element in numList:
+    print(element, end=" ")
+
+print()
+print()
+
+count = 0
+
+while count <= 10:
+    print(f"Count is: {count}")
+    count += 1
