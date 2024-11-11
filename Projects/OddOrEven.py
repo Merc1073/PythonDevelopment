e050af843c0fee4ec9807cb3a230fd299b035df6
= <sergeikoblov2000@gmail.com>
2024-11-11 13:05:29 +0000

    Practice Project: Odd or Even
    Ask user to input a number, and output if their number is odd or even.

---
+++ Projects/OddOrEven.py
@@ -0,0 +1,8 @@
+num = int(input("Enter your number to check if it is odd or even: "))
+
+result = num % 2
+
+if result == 0:
+    print("Your number is even.")
+else:
+    print("Your number is odd")
