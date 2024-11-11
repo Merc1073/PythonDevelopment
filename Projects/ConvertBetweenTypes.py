b034abd769f95d581d4c7ec500ef9534b16643f4
= <sergeikoblov2000@gmail.com>
2024-11-11 13:01:18 +0000

    Practice Project: Convert Between Types
    Create a floating-point number, convert it to an integer, then a string, then a boolean, and output the result after each conversion.

---
+++ Projects/ConvertBetweenTypes.py
@@ -0,0 +1,10 @@
+num1 = float(input("Enter your decimal number: "))
+
+num1 = int(num1)
+print(f"Integer: {num1}.")
+
+num1 = str(num1)
+print(f"String: {num1}.")
+
+num1 = bool(num1)
+print(f"Boolean: {num1}.")
