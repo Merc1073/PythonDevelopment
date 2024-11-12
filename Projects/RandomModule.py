f56e65fd398c56e46866ad10a30a9e332f86766b
= <sergeikoblov2000@gmail.com>
2024-11-12 08:05:50 +0000

    Practice Project: Random Module
    Use the random module to generate a random number between 1 and 10. Print the number.

---
+++ Projects/RandomModule.py
@@ -0,0 +1,22 @@
+"""
+Objective:
+
+Use the random module to generate a random number between 1 and 10. Print the number.
+
+"""
+
+# my attempt
+import random
+
+randomNumber = random.randint(1, 10)
+
+print(randomNumber)
+
+
+
+
+# correct solution
+import random
+
+random_number = random.randint(1, 10)
+print("Random number:", random_number)
